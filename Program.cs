b7ac408a7ffe3c15fe5978471b442bcd222402e8
Rafael Batista <leafarbts@gmail.com>
2020-11-26 02:04:02 -0300

    project init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pattern_matching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
